b7c07078ad6c44746b348970f8697797934ba4dd
themanzi <christophermmanzi@protonmail.com>
2018-06-11 13:02:37 -0500

    stdout works

--- run.py
+++ run.py
@@ -1,17 +1,32 @@
 import json
-import os
-import sys
 
 
-def print_json(data):
-    with os.fdopen(sys.stdout.fileno(), 'wb') as fp:
-        for result in data["results"]:
-            # Write to stdout buffer
-            fp.write(json.dumps(result, ensure_ascii=False).encode())
-            # write null byte after result
-            fp.write(b'\00')
-            # flush to stdout
-            fp.flush()
+def print_json():
+    issue = {
+        'type': 'issue',
+        'check_name': 'Unused Variable',
+        'categories': ['Style'],
+        'description': 'Unused local variable foo',
+        'remediation_points': 50000,
+        'location': {
+            'path': 'db_dir/project_db.py',
+            'positions': {
+                'begin': {
+                    'line': 13,
+                    'column': 1
+                },
+                'end': {
+                    'line': 14,
+                    'column': 5
+                }
+            }
+        },
+        'content': {
+            'body': 'This is a markdown snippet'
+        }
+    }
+
+    print(json.dumps(issue) + '\0')
 
 
 if __name__ == '__main__':
@@ -19,6 +34,7 @@ if __name__ == '__main__':
     # json_data = open("/scripts/results.json").read()
 
     # load to dict
-    data = json.loads(sys.argv[1])
+    # data = json.loads(sys.argv[1])
+    # testing
 
-    print_json(data)
+    print_json()
--- run.sh
+++ run.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-bandit -r -f json /code | sed -r -n -e '/result/,${p}' \
-| sed 's/\"code\"/\"type\": \"issue\",\n    \"content\"/g' \
-| sed 's/issue_text/description/g' | sed 's/],/]/g' \
-| sed 's/\"filename\"/\"categories\": \[\"Security\"\],\n    \"filename\"/g' \
-| sed 's/test_name/check_name/g' | sed 's/line_number/location/g' \
-| sed 's/line_range/other_locations/g' | sed 's/issue_severity/severity/g' \
-| sed 's/^  \"results\": \[$/{\n  \"results\": \[/g' | sed '/^.*\(issue_confidence\|test_id\|check_name\).*$/d' \
-| xargs -0 -I {} python3 /scripts/run.py {}
+bandit -r -f json /code | xargs -0 -I {} python3 /scripts/run.py {}
